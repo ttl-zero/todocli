54a9974f4648de4d7477f7b09cd35df5baa22b41
Gabriel <timetolivezero@proton.me>
2026-03-19 12:13:15 -0300

    feat: adiciona comando check

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ program
 
 program
     .command("add <task>")
-    .description("Add a task to your list")
+    .description("Add new task")
     .action((task) => {
         const tasks = getTasks();
 
-        const nextId = tasks.length > 0 ? Math.max(... tasks.map(t => t.id)) + 1 : 1;
+        const nextId = tasks.length > 0 ? Math.max(...tasks.map(t => t.id)) + 1 : 1;
 
-        tasks.push({id: nextId, content: task});
-        
+        tasks.push({ id: nextId, content: task, check: false });
+
         saveTasks(tasks);
 
         console.log(chalk.green("Task added to your list"));
@@ -38,17 +38,19 @@ program
 
 program
     .command("list")
-    .description("Show all your tasks")
+    .description("List all tasks")
     .action(() => {
         const tasks = getTasks();
-        if(tasks.length === 0) {
+        if (tasks.length === 0) {
             console.log('Task list is empty');
             return;
         }
         console.log(chalk.blue.bold('\n My tasks ᕙ(⇀ ‸↼‶)ᕗ'));
-    tasks.forEach(t => {
-      console.log(chalk.yellow(`${t.id}. ${t.content}`));
-    });
+        tasks.forEach(t => {
+            const status = t.check ? chalk.green('[ ✔ ]') : chalk.red('[   ]');
+            const content = t.check ? chalk.gray.italic(t.content) : chalk.yellow(t.content);
+            console.log(`${chalk.blue(t.id + '.')} ${status} ${content}`);
+        });
     });
 
 program
@@ -57,22 +59,38 @@ program
     .action((id) => {
         let tasks = getTasks();
         const newList = tasks.filter(t => t.id !== parseInt(id));
-        
-        if(tasks.length === newList.length) {
+
+        if (tasks.length === newList.length) {
             console.log(chalk.red('ID not found'));
         } else {
             saveTasks(newList);
-            console.log(chalk.red(`Task ${id} was successfully removed!`));
+            console.log(chalk.red(`Task ${id} was removed!`));
+        }
+    });
+
+program
+    .command("check <id>")
+    .description("Check/Uncheck task")
+    .action((id) => {
+        let tasks = getTasks();
+        const task = tasks.find(t => t.id === Number(id));
+        if (task) {
+            task.check = !task.check;
+            const msg = task.check ? "checked" : "unchecked"
+            console.log(chalk.green(`Task ${chalk.yellow(id)} was marked as ${msg}`));
+            saveTasks(tasks);
+        } else {
+            console.log(chalk.red("Task not found"));
         }
     });
 
 program
     .command("flush")
-    .description("Flushes your todo list")
+    .description("Nuclear option: wipe everything out")
     .action(() => {
         saveTasks([]);
-        console.log(chalk.bgGray(`Your to-do list has been flushed.`));
-        }
+        console.log(chalk.bgRed.white.bold(` ALL TASKS HAVE BEEN WIPED OUT! `))
+    }
     );
 
 program.parse()
